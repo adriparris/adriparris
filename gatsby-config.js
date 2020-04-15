baf6733da0e4074af5ebce1844641e94bdab1fd8
Adri Parris <adri@bayleaf.design>
2020-04-15 16:24:35 +0200

    fixed and working

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,4 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-module.exports = {
-  plugins: [
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-  ],
-}
+module.exports = {}
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,3 +1,5 @@
 import React from "react"
 import "./layout.css"
+import "typeface-lato"
+
 export default ({ children }) => <div>{children}</div>
