0f941c384c5686ddde7214c3021421f45e075133
Rickert Mulder <circlingthesun@gmail.com>
2020-04-27 21:08:12 +0200

    I made you a link

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,37 +1,25 @@
-import React, { Component } from "react"
+import React from "react"
 import "./menu.css"
-import { Link } from "gatsby"
+import Link from "../link"
 import { Flex, Box } from "theme-ui"
 
-class Menu extends React.Component {
-  render() {
-    return (
-      <nav>
-        <Flex
-          className="menutype"
-          sx={{
-            flexDirection: "column",
-          }}
-        >
-          <Link to="/ui-design" activeStyle={{ color: "colors.primary" }}>
-            ui design
-          </Link>
-          <Link to="/ux-design" activeStyle={{ color: "tomato" }}>
-            ux design
-          </Link>
-          <Link to="/web-dev" activeStyle={{ color: "tomato" }}>
-            web development
-          </Link>
-          <Link to="/illustration" activeStyle={{ color: "tomato" }}>
-            illustration
-          </Link>
-          <Link to="/plane" activeStyle={{ color: "tomato" }}>
-            a plane
-          </Link>
-        </Flex>
-      </nav>
-    )
-  }
+const Menu = () => {
+  return (
+    <nav>
+      <Flex
+        className="menutype"
+        sx={{
+          flexDirection: "column",
+        }}
+      >
+        <Link to="/ui-design">ui design</Link>
+        <Link to="/ux-design">ux design</Link>
+        <Link to="/web-dev">web development</Link>
+        <Link to="/illustration">illustration</Link>
+        <Link to="/plane">a plane</Link>
+      </Flex>
+    </nav>
+  )
 }
 
 export default Menu
